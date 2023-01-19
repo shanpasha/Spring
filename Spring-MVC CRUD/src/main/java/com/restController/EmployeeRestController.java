99d17b8f201a76ee0b730b19fb7d672648c58188
new <new@shan>
2023-01-19 10:42:37 +0530

    Spring MVC and AOP With loggers and Spring Restful Web Service

---
+++ Spring-MVC CRUD/src/main/java/com/restController/EmployeeRestController.java
@@ -0,0 +1,60 @@
+package com.restController;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder.In;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.Service.EmployeeeService;
+import com.model.Employee;
+
+@Controller
+public class EmployeeRestController {
+
+	@Autowired
+	EmployeeeService service;
+
+	//@RequestMapping(value = "/getAll/json", produces = "application/json", method = RequestMethod.GET)
+	@GetMapping("/getAll/json")
+	public @ResponseBody List<Employee> getEmp() {
+		return service.getAll();
+	}
+
+//	@RequestMapping(value = "save/json", produces = "application/json", consumes = "application/json", method = RequestMethod.POST)
+	
+	@PostMapping("save/json")
+	public @ResponseBody String save(@RequestBody Employee employee) {
+		service.save(employee);
+
+		return "saved Sucessfully...";
+	}
+
+	//@RequestMapping(value = "/getById/json/{id}", produces = "application/json", consumes = "application/json", method = RequestMethod.GET)
+
+	@GetMapping("/getById/json/{id}")
+	public @ResponseBody Employee getbyId(@PathVariable Integer id) {
+
+		return service.getByID(id);
+
+	}
+
+	//@RequestMapping(value = "/deleteById/json/{id}", method = RequestMethod.DELETE, produces = "application/json", consumes = "application/json")
+	@DeleteMapping("/deleteById/json/{id}")
+	public @ResponseBody String DeleteByID(@PathVariable Integer id) {
+
+		service.delete(id);
+		return "1 Record Deleted Sucessfully...!";
+
+	}
+
+}
